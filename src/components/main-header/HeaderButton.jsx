7f5e5bfb43d616ac4833521b6b01e4664cd060b7
Salman <mohammadalfarisi00@gmail.com>
2021-10-08 16:36:31 +0700

    feat: add HeaderButton

---
+++ src/components/main-header/HeaderButton.jsx
@@ -0,0 +1,39 @@
+const HeaderButton = () => {
+	return (
+		<div className="col-span-2">
+			<button className="bg-accent w-full text-left text-xl font-bold px-4 py-2">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="h-8 w-8 inline-block"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth={2}
+						d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+					/>
+				</svg>
+				<span className="px-4">Find Laptops</span>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					className="h-6 w-6 inline-block"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="white"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth={2}
+						d="M17 8l4 4m0 0l-4 4m4-4H3"
+					/>
+				</svg>
+			</button>
+		</div>
+	);
+};
+
+export default HeaderButton;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import HeaderBody from '../components/main-header/HeaderBody';
+import HeaderButton from '../components/main-header/HeaderButton';
 import NavBar from '../components/main-header/NavBar';
 
 const LandingPage = () => {
@@ -7,7 +8,7 @@ const LandingPage = () => {
 			<header className="grid grid-cols-2 bg-gray-700 h-full px-32">
 				<NavBar />
 				<HeaderBody />
-				<div className="col-span-2">tesxst</div>
+				<HeaderButton />
 			</header>
 		</div>
 	);
