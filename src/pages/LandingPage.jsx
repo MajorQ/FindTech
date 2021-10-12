03bc38d537ed0efcd8fb624e0d8d95c6a834514d
Farhan Almasyhur <farhan.almasyhur@gmail.com>
2021-10-12 17:39:46 +0800

    feat: add SecondInfo

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import MainInfo from '../components/learn-more/MainInfo';
+import SecondInfo from '../components/learn-more/SecondInfo';
 
 const LandingPage = () => {
 	return (
 		<div>
 			<MainInfo />
+			<SecondInfo />
 		</div>
 	);
 };
