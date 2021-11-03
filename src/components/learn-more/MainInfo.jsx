9cc7e5a7966a98c8f2a7d137902f47fae2161faf
Salman <mohammadalfarisi00@gmail.com>
2021-11-04 01:37:35 +0700

    refactor: buttons in LandingPage to RoundedButton

--- src/components/learn-more/MainInfo.jsx
+++ src/components/learn-more/MainInfo.jsx
@@ -1,4 +1,5 @@
 import image_maininfo from '../../assets/images/image_maininfo.png';
+import RoundedButton from '../core/RoundedButton.jsx';
 
 const MainInfo = () => {
 	return (
@@ -14,31 +15,19 @@ const MainInfo = () => {
 					</h2>
 				</div>
 				<div className="max-w-sm">
-					<p className="text-left mb-10 text-gray-600 text-sm">
+					<p className="text-left mb-10 text-gray-600 text-sm leading-relaxed">
 						We gather models from popular lists of laptops in the internet, ones
 						that are available in your local area and recommend them to you
 						through a series of user-friendly questions that will be processed
 						through AI algorithm, so your recommendations will be personalized
 						to your needs.
 					</p>
-					<div className="grid justify-items-start">
-						<button className="bg-black w-1/2 text-left font-bold rounded-tr-xl px-4 py-2">
-							<span className="px-1 text-sm text-white">Learn More</span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-6 w-6 inline-block stroke-current text-accent"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M17 8l4 4m0 0l-4 4m4-4H3"
-								/>
-							</svg>
-						</button>
-					</div>
+					<RoundedButton
+						label="Learn More"
+						arrowColor="text-accent"
+						backgroundColor="bg-black"
+						textColor="text-white"
+					/>
 				</div>
 			</div>
 		</div>
--- src/components/learn-more/SecondInfo.jsx
+++ src/components/learn-more/SecondInfo.jsx
@@ -1,4 +1,5 @@
 import image_secondinfo from '../../assets/images/image_secondinfo.png';
+import RoundedButton from '../core/RoundedButton.jsx';
 
 const SecondInfo = () => {
 	return (
@@ -11,29 +12,17 @@ const SecondInfo = () => {
 					</h2>
 				</div>
 				<div className="max-w-sm">
-					<p className="text-left mb-10 text-gray-600 text-sm">
+					<p className="text-left mb-10 text-gray-600 text-sm leading-relaxed">
 						We work hard to make sure you get the best of the recommendations in
 						the internet. Make yourself at ease with our best picked laptop
 						recommendations and trusted sources information
 					</p>
-					<div className="grid justify-items-start">
-						<button className="bg-black w-1/2 text-left font-bold rounded-tr-xl px-4 py-2">
-							<span className="px-1 text-sm text-white">Learn More</span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-6 w-6 inline-block stroke-current text-accent"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M17 8l4 4m0 0l-4 4m4-4H3"
-								/>
-							</svg>
-						</button>
-					</div>
+					<RoundedButton
+						label="Learn More"
+						arrowColor="text-accent"
+						backgroundColor="bg-black"
+						textColor="text-white"
+					/>
 				</div>
 			</div>
 			<div className="grid justify-end">
