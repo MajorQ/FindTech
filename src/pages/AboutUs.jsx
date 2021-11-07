76bf7dbc7cfc6923454267ad3dfba9a74d17fdf2
Farhan Almasyhur <farhan.almasyhur@gmail.com>
2021-11-07 17:38:09 +0800

    fix: bg navbar about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -6,7 +6,7 @@ import AboutNavBar from '../components/about-us/NavBar';
 const AboutUs = () => {
 	return (
 		<div>
-			<header className="grid grid-cols-2 gap-y-16 px-80 bg-gradient-to-r from-black to-darkaccent">
+			<header className="grid grid-cols-2 gap-y-16 px-80 bg-gradient-to-r from-black to-accent_dark">
 				<AboutNavBar />
 			</header>
 			<div
