852bd98aa08cd669d40f4d966b60cf8d34a01786
Salman <mohammadalfarisi00@gmail.com>
2021-11-09 13:01:11 +0700

    feat: landing page footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,27 @@
+import logo from '../../assets/images/logo.svg';
+import RoundedButton from '../core/RoundedButton';
+
 const Footer = () => {
-	return <div></div>;
+	return (
+		<div className="w-screen bg-gradient-to-r from-black to-accent_dark divide-solid divide-y-2 divide-gray-800 px-32 py-24">
+			<div className="flex justify-between items-center mb-16">
+				<h2 className="text-4xl text-left  font-bold tracking-tighter my-5">
+					<span className="text-white">Make your dream laptop</span>
+					<span className="text-accent"> come true </span>
+				</h2>
+				<RoundedButton
+					label="Find yours now"
+					arrowColor="text-white"
+					backgroundColor="bg-accent"
+					textColor="text-black"
+				/>
+			</div>
+
+			<div className="pt-16">
+				<img className="mx-auto" src={logo} alt="Findtech Company Logo" />
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
