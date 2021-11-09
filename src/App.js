41a8f06897ff8ad44603cb8ba93c21b3cac15f4d
Salman <mohammadalfarisi00@gmail.com>
2021-11-09 13:22:59 +0700

    chore: removed NavBar in about-us folder

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 
 import './App.css';
-import AboutUs from './pages/AboutUs';
+import AboutUsPage from './pages/AboutUsPage';
 import BrowsePage from './pages/BrowsePage';
+import QuestionsPage from './pages/QuestionsPage';
 
 const App = () => {
 	return (
@@ -12,8 +13,9 @@ const App = () => {
 			<BrowserRouter>
 				<Switch>
 					<Route exact path="/" component={LandingPage} />
-					<Route path="/about-us" component={AboutUs} />
+					<Route path="/about-us" component={AboutUsPage} />
 					<Route path="/browse" component={BrowsePage} />
+					<Route path="/questions" component={QuestionsPage} />
 				</Switch>
 			</BrowserRouter>
 		</div>
